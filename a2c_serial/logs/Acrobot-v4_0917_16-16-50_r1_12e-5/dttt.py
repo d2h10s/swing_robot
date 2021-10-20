50062ee22b6614f2683cbee97380998bc91715b5
d2h10s <ecanus9@gmail.com>
2021-10-20 16:04:52 +0900

    delete trash logs2

--- a2c_serial/logs/Acrobot-v4_0917_16-16-50_r1_12e-5/dttt.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-d = {}
-with open('episode-reward-loss-freq-sigma.txt', 'r') as f:
-    lines = f.readlines()
-    for line in lines:
-        episode, reward, loss, freq, sigma = map(float, line.split())
-        d[int(episode)] = {'reward':reward, 'loss':loss, 'freq':freq, 'sigma':sigma}
